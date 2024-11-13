a67186720c2f8357dbeca7b57d6f029c9131e50e
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-11-13 16:37:24 +0200

    better settings

--- components/Settings2.vue
+++ components/Settings2.vue
@@ -14,11 +14,18 @@ const settings = useVModel(props, "modelValue", emit);
     <div v-for="setting in settings">
       <div v-if="setting.type === 'color'" class="flex flex-col gap-2">
         <label class="">{{ setting.title }}</label>
-        <input type="color" v-model="setting.value" />
+        <input
+          type="color"
+          v-model="setting.value"
+          class="ring-2 ring-white appearance-none h-4 w-full rounded [&::-webkit-color-swatch-wrapper]:p-0 [&::-webkit-color-swatch]:border-none"
+        />
       </div>
       <div v-if="setting.type === 'textarea'" class="flex flex-col gap-2">
         <label>{{ setting.title }} {{ setting.value }}</label>
-        <textarea v-model="setting.value" />
+        <textarea
+          v-model="setting.value"
+          class="bg-black border-2 rounded px-2 py-1 outline-none"
+        />
       </div>
       <div v-if="setting.type === 'range'" class="flex flex-col gap-2">
         <label>{{ setting.title }} {{ setting.value }}</label>
@@ -28,6 +35,7 @@ const settings = useVModel(props, "modelValue", emit);
           :min="setting.min"
           :max="setting.max"
           :step="setting.step"
+          class="accent-neutral-300 block w-full bg-white h-[2px] relative outline-none my-2 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:border-2 [&::-webkit-slider-thumb]:border-white [&::-webkit-slider-thumb]:h-6 [&::-webkit-slider-thumb]:w-6 [&::-webkit-slider-thumb]:bg-gray-500 [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:rounded-full"
         />
       </div>
     </div>
